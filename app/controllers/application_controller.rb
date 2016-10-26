c6f155cdeac2439514895e21cb93aafba926ae9a
Russell Malenchek <malenchek@gmail.com>
2016-10-26 17:41:57 -0400

    Updated Bundler and changed text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "Hello, Cleveland!! GO INDIANS"
   end
 end
+
